9871bb5b71d4f4db5d820aa9a1ab8944ff880c6e
Mina Park <kyminp@gmail.com>
2022-09-12 17:12:19 +0900

    add:cascade, 고아객체

---
+++ src/main/java/hellojpa/cascade/jpaMain.java
@@ -0,0 +1,44 @@
+package hellojpa.cascade;
+
+import hellojpa.mapping.MemberMapping;
+import hellojpa.mapping.TeamMapping;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Child child1 = new Child();
+            Child child2 = new Child();
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            //각각 persist하지 않아도 parent cascade를 all로 설정하면 연관된 child도 영속상태가 됨
+            em.persist(parent);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0); //첫번째 자식 삭제
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
