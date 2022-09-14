4d7b9a28562f54ace152c9070d1f4e91f06e929d
Mina Park <kyminp@gmail.com>
2022-09-14 23:10:38 +0900

    add:Criteria 예시

--- src/main/java/hellojpa/mapping/jpaMainMapping.java
+++ src/main/java/hellojpa/mapping/jpaMainMapping.java
@@ -3,6 +3,9 @@ package hellojpa.mapping;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -15,7 +18,28 @@ public class jpaMainMapping {
         tx.begin();
 
         try {
-            List<MemberMapping> result = em.createQuery("select m from MemberMapping m where m.username like '%kim%'", MemberMapping.class).getResultList();
+
+            //1) JPQL 사용 예시
+            //List<MemberMapping> result = em.createQuery("select m from MemberMapping m where m.username like '%kim%'", MemberMapping.class).getResultList();
+
+            //2) Criteria 사용 예시
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<MemberMapping> query = cb.createQuery(MemberMapping.class);
+
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<MemberMapping> m = query.from(MemberMapping.class);
+
+            //쿼리 생성
+            CriteriaQuery<MemberMapping> cq = query.select(m);
+
+            //동적쿼리 생성에 편리
+            String username = "test";
+            if (username != null) {
+                cq = cq.where(cb.equal(m.get("username"),"kim"));
+            }
+
+            List<MemberMapping> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
